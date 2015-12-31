3eb893250720edf5f3441ec38b330060bab724fc
61352151511 <cassboys@hotmail.com>
2015-12-30 22:00:29 -0400

    1.8.9 Port

--- src/main/java/com/sixonethree/randomutilities/reference/Reference.java
+++ src/main/java/com/sixonethree/randomutilities/reference/Reference.java
@@ -3,7 +3,7 @@ package com.sixonethree.randomutilities.reference;
 public class Reference {
 	public static final String MOD_ID = "RandomUtilities";
 	public static final String MOD_NAME = "Random Utilities";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.1.1";
 	public static final String CLIENT_PROXY = "com.sixonethree.randomutilities.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.sixonethree.randomutilities.proxy.CommonProxy";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
